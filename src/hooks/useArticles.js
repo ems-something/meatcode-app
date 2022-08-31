f90f20b0781b05122a936c0d88851afd137d847e
mguevarashokworks <mguevara@shokworks.io>
2022-08-31 11:22:31 -0400

    Managing state
    Custom hook to manage state of app. Calls to all articles and articles filtered

---
+++ src/hooks/useArticles.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+export default function useArticles() {
+    const [articles, setArticles] = useState([]);
+    const [filter, setFilter] = useState("Todos");
+    const baseUrl = "https://5eed24da4cbc340016330f0d.mockapi.io/api/articles";
+
+    useEffect(() => {    
+        getArticles();
+        return () => {};
+    }, [filter]);
+
+    const getArticles = () => {
+        setArticles([]);
+        if (filter === "Todos")
+            axios.get(baseUrl)
+                .then((res) => {
+                    setArticles(res);
+                })
+                .catch(error => console.error(""));
+        else {
+            axios.get(`${baseUrl}?filter=${filter}`)
+                .then((res) => {
+                    setArticles(res);
+                })
+                .catch(error => console.error(""));
+        }
+    };
+
+    return { articles, filter, setFilter };
+}
